55cdd12bd374498a84b959055b45bee64252d897
Hitendra Singh <hitendra_1985@yahoo.com>
2013-07-21 18:45:04 +0530

    changes made for select

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -10,11 +10,16 @@ class Pin < ActiveRecord::Base
   #Checks if user_id is present for uploading a a pin
   validates :user_id, presence: true
 
+  validates :category, presence: true
+
   #Pins belongs to ONE user
 	belongs_to :user
 
 	#Pin belongs to ONE Board
 	belongs_to :board
+
+  #Pin belongs to ONE Category
+  belongs_to :category
 
   #For paperclip
   has_attached_file :image, styles: { medium: "320x240>" }
